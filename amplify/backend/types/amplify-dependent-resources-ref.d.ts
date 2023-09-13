33d815c31f53957a1942cdc4bbb2766cf70e9da2
Vince Khoa Dinh <39015239+vincedinh@users.noreply.github.com>
2023-09-12 17:19:20 -0700

    Added front end code and graphql api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraftql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraftqldbf31452": {
       "AppClientID": "string",
--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css"
+import { API } from "aws-amplify";
+
 import {
   withAuthenticator,
   Button,
+  Flex,
   Heading,
+  Text,
   Image,
   View,
   Card,
+  TextField,
 } from "@aws-amplify/ui-react";
 
-function App({ signOut }) {
-  return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
+import { listTodos as listNotes } from "./graphql/queries";
+import {
+  createTodo as createNoteMutation,
+  deleteTodo as deleteNoteMutation,
+} from "./graphql/mutations";
+
+const App = ({ signOut }) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+async function fetchNotes() {
+  const apiData = await API.graphql({ query: listNotes });
+  const notesFromAPI = apiData.data.listTodos.items;
+  setNotes(notesFromAPI);
+}
+
+async function createNote(event) {
+  event.preventDefault();
+  const form = new FormData(event.target);
+  const data = {
+    name: form.get("name"),
+    description: form.get("description"),
+  }
+  await API.graphql({
+    query: createNoteMutation,
+    variables: { input: data },
+  });
+  fetchNotes();
+  event.target.reset();
+}
+
+async function deleteNote({id}) {
+  const newNotes = notes.filter((note) => note.id !== id);
+  setNotes(newNotes)
+  await API.graphql({
+    query: deleteNoteMutation,
+    variables: { input: { id }},
+  });
+}
+
+return (
+  <View className="App">
+    <Heading level={1}>My Notes App</Heading>
+    <View as="form" margin="3rem 0" onSubmit={createNote}>
+      <Flex direction="row" justifyContent="center">
+        <TextField 
+        name="name"
+        placeholder="Note Name"
+        label="Note Name"
+        labelHidden
+        variation="quiet"
+        required
+        />
+        <TextField
+        name="description"
+        placeholder="Note Description"
+        label="Note Description"
+        labelHidden
+        variation="quiet"
+        required
+        />
+        <Button type="submit" variation="primary">
+          Create Note
+        </Button>
+      </Flex>
     </View>
-  );
+    <View margin="3rem 0">
+      {notes.map((note) => {
+        <Flex key={note.id || note.name}
+        direction = "row"
+        justifyContent="center"
+        alignItems="center"
+        >
+          <Text as="strong" fontWeight={700}>
+            {note.name}
+          </Text>
+          <Text as="span">{note.description}</Text>
+          <Button variation="link" onClick={()=>deleteNote(note)}>
+            Delete Note
+          </Button>
+        </Flex>
+      })}
+    </View>
+  </View>
+)
+
 }
 
 export default withAuthenticator(App);
